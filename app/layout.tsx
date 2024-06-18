b7a874365ca3d01b89831715b1322c28f055ab2e
iGauravSingh <poetonroad@gmail.com>
2024-06-18 17:41:00 +0530

    try 4 to change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'Webcos',
   description: 'The official Webcos home page.',
-  // icons: {
-  //   icon: '/favicon.ico',
-  // },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
